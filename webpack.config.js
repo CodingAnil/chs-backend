90ec52ecf6f95379c509cfe680de0512a1071303
aCoding <programmeranil36@gmail.com>
2025-02-16 16:27:26 +0530

    updtaed config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 // webpack.config.js
 const path = require("path");
+const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
   entry: "./src/server.js",
@@ -7,18 +8,28 @@ module.exports = {
     filename: "bundle.js", // Output bundle file
     path: path.resolve(__dirname, "dist"), // Output directory
   },
-  target: "node", // Since you're building a Node.js app
-  mode: "production", // You can use 'development' or 'production'
+  target: "node", // Building for Node.js environment
+  mode: "production", // Use 'production' for optimized output or 'development' for debugging
+  externals: [nodeExternals()], // Exclude node_modules from bundling
   module: {
     rules: [
       {
         test: /\.html$/,
-        use: ["html-loader"],
+        use: ["html-loader"], // Load HTML files
       },
     ],
   },
-  externals: {
-    _http_common: "commonjs _http_common",
+  resolve: {
+    fallback: {
+      fs: false,
+      path: false,
+      net: false,
+      tls: false,
+      crypto: false, // Ignore server-side modules not needed in the bundle
+    },
   },
-  ignoreWarnings: [/node_modules/],
+  stats: {
+    errorDetails: true, // Show detailed error messages
+  },
+  ignoreWarnings: [/node_modules/], // Ignore warnings from node_modules
 };
